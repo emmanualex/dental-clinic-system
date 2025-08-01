84bbd9390a7db6e8346b218b1ea3219dad001517
Emmanuel Kinyua <2401363@students.kcau.ac.ke>
2025-08-01 10:05:59 +0300

    Update contactcontroller.js

--- backend/controllers/contactcontroller.js
+++ backend/controllers/contactcontroller.js
@@ -1,1 +1,26 @@
+const Contact = require('../models/Contact');
 
+exports.createContact = async (req, res) => {
+    try {
+        const { name, email, subject, message } = req.body;
+        
+        const contact = new Contact({
+            name,
+            email,
+            subject,
+            message
+        });
+
+        const savedContact = await contact.save();
+        res.status(201).json({
+            success: true,
+            message: 'Message sent successfully!',
+             savedContact
+        });
+    } catch (error) {
+        res.status(400).json({
+            success: false,
+            message: error.message
+        });
+    }
+};
